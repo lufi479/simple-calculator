ba9370dbeadc604ee860b44dd6aa27f7353aa2ab
lufi479 <135998525+lufi479@users.noreply.github.com>
2023-06-08 11:19:43 -0700

    Add functionality for calculating the expressions and show it on the screen

--- app.js
+++ app.js
@@ -17,48 +17,89 @@ function multiply(first,second){
 function operate(operator, first, second){
     switch (operator){
         case '+':
-            add(first, second);
+            return add(first, second);
             break;
         case '-':
-            subtract(first,second);
+            return subtract(first,second);
             break;
-        case '/':
-            divide(first, second);
+        case '÷':
+            return divide(first, second);
             break;
-        case '*':
-            multiply(first,second);
+        case 'x':
+            return multiply(first,second);
             break;
     }
 
 }
 
-let firstNum = 0;
-
+let prevNum = 0;
 let operator = '+';
-
-let secondNum = 0;
+let currNum = 0;
 
 let currDecimal = false;
+let choseOp = false;
 
-const currNum = document.querySelector('.screen-current');
+const screenCurrNum = document.querySelector('.screen-current');
+const screenPrevNum = document.querySelector('.screen-prev');
+const equalsBtn = document.querySelector('#equals');
+equalsBtn.addEventListener('click', equals);
 
 const numbers = document.querySelectorAll('[data-number]');
 numbers.forEach(num => num.addEventListener('click', addNum));
 
+const operators = document.querySelectorAll('[data-operator]');
+operators.forEach(op => op.addEventListener('click', chooseOperator));
+
+function equals(e){
+    if (choseOp === true){
+        currNum = Number(screenCurrNum.innerText);
+        prevNum = operate(operator,prevNum, currNum);
+        screenCurrNum.innerText = prevNum;
+        currNum = 0;
+        choseOp = false;
+    }
+
+}
+
 function addNum(e){
     if (e.target.innerText === '.') {
         if (currDecimal === false){
-            currNum.innerText += e.target.innerText;
+            screenCurrNum.innerText += e.target.innerText;
             currDecimal = true;
         }
     }
     else {
-        if (currNum.innerText !== '0'){
-            currNum.innerText += e.target.innerText;
+        if (screenCurrNum.innerText !== '0'){
+            if (currNum === 0){
+                screenCurrNum.innerText = e.target.innerText;
+                screenPrevNum.innerText = '';
+                prevNum = 0;
+            }
+            else {
+                screenCurrNum.innerText += e.target.innerText;
+            }
         }
         else {
-            currNum.innerText = e.target.innerText;
+            screenCurrNum.innerText = e.target.innerText;
         }
+        currNum = Number(screenCurrNum.innerText);
     }
+    
+}
+
+function chooseOperator(e){
+    if (choseOp === false){
+        prevNum = Number(screenCurrNum.innerText);
+        currDecimal = false;
+        screenPrevNum.innerText = prevNum;
+        console.log(prevNum);
+        screenCurrNum.innerText = '0'
+        choseOp = true;
+    }
+    else {
+        equals(e);
+        chooseOperator(e);
+    }
+    operator = e.target.innerText;
     
 }
