f894a95d031554441030dc64cee317f0a60dcac2
lufi479 <135998525+lufi479@users.noreply.github.com>
2023-06-09 02:54:56 -0700

    Add functionality for the all-clear, clear, and sign buttons

--- app.js
+++ app.js
@@ -32,59 +32,30 @@ function operate(operator, first, second){
 
 }
 
-let prevNum = 0;
-let operator = '+';
-let currNum = 0;
-
-let currDecimal = false;
-let choseOp = false;
-
-const screenCurrNum = document.querySelector('.screen-current');
-const screenPrevNum = document.querySelector('.screen-prev');
-const equalsBtn = document.querySelector('#equals');
-equalsBtn.addEventListener('click', equals);
-
-const numbers = document.querySelectorAll('[data-number]');
-numbers.forEach(num => num.addEventListener('click', addNum));
-
-const operators = document.querySelectorAll('[data-operator]');
-operators.forEach(op => op.addEventListener('click', chooseOperator));
-
-function equals(e){
-    if (choseOp === true){
-        currNum = Number(screenCurrNum.innerText);
-        prevNum = operate(operator,prevNum, currNum);
-        screenCurrNum.innerText = prevNum;
-        currNum = 0;
-        choseOp = false;
-    }
-
+function allClear(e){
+    prevNum = 0;
+    currNum = 0;
+    choseOp = false;
+    operator = '+';
+    screenPrevNum.innerText = '';
+    clear(e);
 }
 
-function addNum(e){
-    if (e.target.innerText === '.') {
-        if (currDecimal === false){
-            screenCurrNum.innerText += e.target.innerText;
-            currDecimal = true;
-        }
-    }
-    else {
-        if (screenCurrNum.innerText !== '0'){
-            if (currNum === 0){
-                screenCurrNum.innerText = e.target.innerText;
-                screenPrevNum.innerText = '';
-                prevNum = 0;
-            }
-            else {
-                screenCurrNum.innerText += e.target.innerText;
-            }
+function clear(e){
+    screenCurrNum.innerText = '0';
+    currDecimal = false;
+}
+
+function changeSign(e){
+    if (screenCurrNum.innerText !== '0'){
+        if (screenCurrNum.innerText.charAt(0) === '-'){
+            screenCurrNum.innerText = screenCurrNum.innerText.substring(1);
         }
         else {
-            screenCurrNum.innerText = e.target.innerText;
+            screenCurrNum.innerText = '-' + screenCurrNum.innerText;
         }
-        currNum = Number(screenCurrNum.innerText);
     }
-    
+
 }
 
 function chooseOperator(e){
@@ -103,3 +74,69 @@ function chooseOperator(e){
     operator = e.target.innerText;
     
 }
+
+function equals(e){
+    if (choseOp === true){
+        currNum = Number(screenCurrNum.innerText);
+        prevNum = operate(operator,prevNum, currNum);
+        screenCurrNum.innerText = prevNum;
+        currNum = 0;
+        choseOp = false;
+        currDecimal = false;
+    }
+
+}
+
+function addNum(e){
+    if (e.target.innerText === '.') {
+        if (currDecimal === false){
+            screenCurrNum.innerText += e.target.innerText;
+            currDecimal = true;
+        }
+    }
+    else {
+        if (screenCurrNum.innerText !== '0'){
+            if (currNum === 0 && currDecimal === false){
+                screenCurrNum.innerText = e.target.innerText;
+                screenPrevNum.innerText = '';
+                prevNum = 0;
+            }
+            else {
+                screenCurrNum.innerText += e.target.innerText;
+            }
+        }
+        else {
+            screenCurrNum.innerText = e.target.innerText;
+        }
+        currNum = Number(screenCurrNum.innerText);
+    }
+    
+}
+
+let prevNum = 0;
+let operator = '+';
+let currNum = 0;
+
+let currDecimal = false;
+let choseOp = false;
+
+const screenCurrNum = document.querySelector('.screen-current');
+const screenPrevNum = document.querySelector('.screen-prev');
+
+const acBtn = document.querySelector('#all-clear');
+acBtn.addEventListener('click', allClear);
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', clear);
+
+const equalsBtn = document.querySelector('#equals');
+equalsBtn.addEventListener('click', equals);
+
+const signBtn = document.querySelector('#sign');
+signBtn.addEventListener('click', changeSign);
+
+const numbers = document.querySelectorAll('[data-number]');
+numbers.forEach(num => num.addEventListener('click', addNum));
+
+const operators = document.querySelectorAll('[data-operator]');
+operators.forEach(op => op.addEventListener('click', chooseOperator));
